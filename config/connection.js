e6dbcc1a73bc295a855b497601006600622c7251
git geek <cout.1.endl@gmail.com>
2022-10-12 07:14:20 -0400

    utils added

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,20 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("tech_talk_db", "root", "helloDBCooper", {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3306,
-  dialectOptions: {},
-});
-
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+      dialectOptions: {},
+    }
+  );
+}
 module.exports = sequelize;
